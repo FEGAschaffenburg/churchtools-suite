7ade4fe1e9c177dc796295a7c34040c14377ffdd
Vereinsring Wasserlos <info@vereinsring-wasserlos.de>
2026-01-04 18:31:56 +0100

    v0.10.3.52: Toggles funktionieren jetzt auch bei Presets (wurden vorher nur im Standard-Modus übergeben)

--- includes/class-churchtools-suite-elementor-widget.php
+++ includes/class-churchtools-suite-elementor-widget.php
@@ -731,6 +731,14 @@ class ChurchTools_Suite_Elementor_Widget extends \Elementor\Widget_Base {
 			'view' => $view,
 		];
 		
+		// v0.10.3.52: Anzeige-Toggles IMMER übergeben (auch bei Presets)
+		$atts['enable_modal'] = ( ! empty( $settings['enable_modal'] ) && $settings['enable_modal'] === 'yes' ) ? 'true' : 'false';
+		$atts['show_description'] = ( isset( $settings['show_description'] ) && $settings['show_description'] === 'yes' ) ? 'true' : 'false';
+		$atts['show_location'] = ( isset( $settings['show_location'] ) && $settings['show_location'] === 'yes' ) ? 'true' : 'false';
+		$atts['show_services'] = ( isset( $settings['show_services'] ) && $settings['show_services'] === 'yes' ) ? 'true' : 'false';
+		$atts['show_calendar_name'] = ( isset( $settings['show_calendar_name'] ) && $settings['show_calendar_name'] === 'yes' ) ? 'true' : 'false';
+		$atts['show_time'] = ( isset( $settings['show_time'] ) && $settings['show_time'] === 'yes' ) ? 'true' : 'false';
+		
 		// Add calendar selection nur bei Standard-Modus
 		if ( $preset_source === 'standard' ) {
 			$atts['calendar'] = ! empty( $selected_calendar_ids ) ? implode( ',', $selected_calendar_ids ) : '';
@@ -740,17 +748,6 @@ class ChurchTools_Suite_Elementor_Widget extends \Elementor\Widget_Base {
 				$atts['limit'] = absint( $settings['limit'] );
 			}
 			
-			// Click-to-Details Modal (v0.10.2.9)
-			$atts['enable_modal'] = ( ! empty( $settings['enable_modal'] ) && $settings['enable_modal'] === 'yes' ) ? 'true' : 'false';
-			
-			// Sprint 1: Anzeige-Parameter (v0.10.3.51: Als String 'true'/'false' für Template-Kompatibilität)
-		$atts['show_description'] = ( isset( $settings['show_description'] ) && $settings['show_description'] === 'yes' ) ? 'true' : 'false';
-		$atts['show_location'] = ( isset( $settings['show_location'] ) && $settings['show_location'] === 'yes' ) ? 'true' : 'false';
-		
-		// Sprint 3: Weitere Anzeige-Parameter (v0.10.3.51: Als String für Template-Kompatibilität)
-		$atts['show_services'] = ( isset( $settings['show_services'] ) && $settings['show_services'] === 'yes' ) ? 'true' : 'false';
-		$atts['show_calendar_name'] = ( isset( $settings['show_calendar_name'] ) && $settings['show_calendar_name'] === 'yes' ) ? 'true' : 'false';
-		$atts['show_time'] = ( isset( $settings['show_time'] ) && $settings['show_time'] === 'yes' ) ? 'true' : 'false';
 			// Sprint 4: Filter-Parameter
 			if ( isset( $settings['order'] ) ) {
 				$atts['order'] = sanitize_text_field( $settings['order'] );
@@ -766,7 +763,6 @@ class ChurchTools_Suite_Elementor_Widget extends \Elementor\Widget_Base {
 			if ( isset( $settings['columns'] ) ) {
 				$atts['columns'] = absint( $settings['columns'] );
 			}
-			
 		}
 		
 		// Call appropriate shortcode
