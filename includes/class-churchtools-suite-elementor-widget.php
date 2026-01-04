475b18d5476587eefa2cc5da9a5a33e101cd22a9
Vereinsring Wasserlos <info@vereinsring-wasserlos.de>
2026-01-04 17:48:53 +0100

    v0.10.3.45: Elementor Toggle Bugfix - isset statt !empty für korrekte false-Werte

--- includes/class-churchtools-suite-elementor-widget.php
+++ includes/class-churchtools-suite-elementor-widget.php
@@ -746,14 +746,13 @@ class ChurchTools_Suite_Elementor_Widget extends \Elementor\Widget_Base {
 			$atts['enable_modal'] = ( ! empty( $settings['enable_modal'] ) && $settings['enable_modal'] === 'yes' ) ? 'true' : 'false';
 			
 			// Sprint 1: Anzeige-Parameter
-			$atts['show_description'] = ( ! empty( $settings['show_description'] ) && $settings['show_description'] === 'yes' );
-			$atts['show_location'] = ( ! empty( $settings['show_location'] ) && $settings['show_location'] === 'yes' );
-			
-			// Sprint 3: Weitere Anzeige-Parameter
-			$atts['show_services'] = ( ! empty( $settings['show_services'] ) && $settings['show_services'] === 'yes' );
-			$atts['show_calendar_name'] = ( ! empty( $settings['show_calendar_name'] ) && $settings['show_calendar_name'] === 'yes' );
-			$atts['show_time'] = ( ! empty( $settings['show_time'] ) && $settings['show_time'] === 'yes' );
-			
+		$atts['show_description'] = ( isset( $settings['show_description'] ) && $settings['show_description'] === 'yes' );
+		$atts['show_location'] = ( isset( $settings['show_location'] ) && $settings['show_location'] === 'yes' );
+		
+		// Sprint 3: Weitere Anzeige-Parameter (v0.10.3.45: isset statt !empty für korrekte false-Werte)
+		$atts['show_services'] = ( isset( $settings['show_services'] ) && $settings['show_services'] === 'yes' );
+		$atts['show_calendar_name'] = ( isset( $settings['show_calendar_name'] ) && $settings['show_calendar_name'] === 'yes' );
+		$atts['show_time'] = ( isset( $settings['show_time'] ) && $settings['show_time'] === 'yes' );
 			// Sprint 4: Filter-Parameter
 			if ( isset( $settings['order'] ) ) {
 				$atts['order'] = sanitize_text_field( $settings['order'] );
