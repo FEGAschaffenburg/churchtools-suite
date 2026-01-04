51fa8a2209ae8f9d76b028ad9fcae9d0644a0a93
Vereinsring Wasserlos <info@vereinsring-wasserlos.de>
2026-01-04 17:50:39 +0100

    v0.10.3.46: Grid Colorful Toggles aktiviert (show_services, show_calendar_name)

--- templates/grid/colorful.php
+++ templates/grid/colorful.php
@@ -21,6 +21,8 @@ $columns = isset( $args['columns'] ) ? absint( $args['columns'] ) : 3;
 $show_time = isset( $args['show_time'] ) ? ChurchTools_Suite_Shortcodes::parse_boolean( $args['show_time'] ) : true;
 $show_location = isset( $args['show_location'] ) ? ChurchTools_Suite_Shortcodes::parse_boolean( $args['show_location'] ) : true;
 $show_description = isset( $args['show_description'] ) ? ChurchTools_Suite_Shortcodes::parse_boolean( $args['show_description'] ) : true;
+$show_services = isset( $args['show_services'] ) ? ChurchTools_Suite_Shortcodes::parse_boolean( $args['show_services'] ) : true;
+$show_calendar_name = isset( $args['show_calendar_name'] ) ? ChurchTools_Suite_Shortcodes::parse_boolean( $args['show_calendar_name'] ) : false;
 ?>
 
 <div class="churchtools-suite-wrapper">
