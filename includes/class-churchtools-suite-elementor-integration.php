626506c6ca0770389c612ba329a7f04f975d1c06
FEGAschaffenburg <plugin@feg-aschaffenburg.de>
2026-01-17 18:37:36 +0100

    Debug: Elementor Integration Logging + Init-Fix

--- includes/class-churchtools-suite-elementor-integration.php
+++ includes/class-churchtools-suite-elementor-integration.php
@@ -23,12 +23,16 @@ class ChurchTools_Suite_Elementor_Integration {
 	 * @since 1.0.4.0
 	 */
 	public static function init() {
+		error_log( '[ChurchTools Elementor] Integration init() called' );
+		
 		// Load the widget class
 		require_once CHURCHTOOLS_SUITE_PATH . 'includes/elementor/class-churchtools-suite-elementor-events-widget.php';
+		error_log( '[ChurchTools Elementor] Widget class file loaded' );
 		
 		// Register hooks
 		add_action( 'elementor/elements/categories_registered', [ __CLASS__, 'register_category' ], 10, 1 );
 		add_action( 'elementor/widgets/register', [ __CLASS__, 'register_widget' ], 10, 1 );
+		error_log( '[ChurchTools Elementor] Hooks registered' );
 	}
 	
 	/**
@@ -38,6 +42,7 @@ class ChurchTools_Suite_Elementor_Integration {
 	 * @since 1.0.4.0
 	 */
 	public static function register_category( $elements_manager ) {
+		error_log( '[ChurchTools Elementor] register_category() called' );
 		$elements_manager->add_category(
 			'churchtools-suite',
 			[
@@ -45,6 +50,7 @@ class ChurchTools_Suite_Elementor_Integration {
 				'icon' => 'fa fa-calendar-alt',
 			]
 		);
+		error_log( '[ChurchTools Elementor] Category registered: churchtools-suite' );
 	}
 	
 	/**
@@ -54,16 +60,25 @@ class ChurchTools_Suite_Elementor_Integration {
 	 * @since 1.0.4.0
 	 */
 	public static function register_widget( $widgets_manager ) {
+		error_log( '[ChurchTools Elementor] register_widget() called' );
+		error_log( '[ChurchTools Elementor] Widget class exists: ' . ( class_exists( 'ChurchTools_Suite_Elementor_Events_Widget' ) ? 'YES' : 'NO' ) );
+		
 		if ( class_exists( 'ChurchTools_Suite_Elementor_Events_Widget' ) ) {
-			$widgets_manager->register( new ChurchTools_Suite_Elementor_Events_Widget() );
+			try {
+				$widget = new ChurchTools_Suite_Elementor_Events_Widget();
+				$widgets_manager->register( $widget );
+				error_log( '[ChurchTools Elementor] Widget registered successfully: ' . $widget->get_name() );
+			} catch ( Exception $e ) {
+				error_log( '[ChurchTools Elementor] ERROR registering widget: ' . $e->getMessage() );
+			}
 		}
 	}
 }
 
-// Initialize if Elementor is active
-if ( did_action( 'elementor/loaded' ) ) {
-	ChurchTools_Suite_Elementor_Integration::init();
-} else {
-	// Register initialization for when Elementor loads
-	add_action( 'elementor/loaded', [ 'ChurchTools_Suite_Elementor_Integration', 'init' ] );
-}
+// Initialize immediately - Elementor is already loaded when this file is included
+error_log( '[ChurchTools Elementor] Integration file loaded' );
+error_log( '[ChurchTools Elementor] elementor/loaded action fired: ' . ( did_action( 'elementor/loaded' ) ? 'YES' : 'NO' ) );
+error_log( '[ChurchTools Elementor] Elementor class exists: ' . ( class_exists( '\Elementor\Plugin' ) ? 'YES' : 'NO' ) );
+
+// Always init immediately since we're loaded after Elementor
+ChurchTools_Suite_Elementor_Integration::init();
