0da939db2c3d4b894abe69821db3be783152c3c9
FEG Development <dev@feg-aschaffenburg.de>
2026-08-24 11:03:42 +0200

    Fix reports overview access

--- addons/churchtools-suite-posts-sync/includes/class-cts-posts-sync-admin.php
+++ addons/churchtools-suite-posts-sync/includes/class-cts-posts-sync-admin.php
@@ -70,7 +70,7 @@ class ChurchTools_Suite_Posts_Sync_Admin {
 		}
 
 		add_submenu_page(
-			     null,
+			     'churchtools-suite',
 			__( 'ChurchTools Übersicht Berichte', 'churchtools-suite-posts-sync' ),
 			__( '📝 ChurchTools Berichte', 'churchtools-suite-posts-sync' ),
 			'manage_churchtools_suite',
